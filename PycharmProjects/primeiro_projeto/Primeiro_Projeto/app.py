9bab30d66a614d84f101411d460cbccb3068adff
Antonio Erick <147534096+AntonioErick@users.noreply.github.com>
2024-11-22 10:29:44 -0300

    Delete PycharmProjects/primeiro_projeto/Primeiro_Projeto directory

--- PycharmProjects/primeiro_projeto/Primeiro_Projeto/app.py
+++
@@ -1,8 +0,0 @@
-import eel
-
-eel.init('web')
-
-#try:
-eel.start(  'index.html', size=(850, 400), port=0)
-#except(SystemExit, MemoryError, KeyboardInterrupt):
-#    print("Program Exit, Save Logs if Needed")
